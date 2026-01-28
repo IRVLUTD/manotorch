update code to fix issues of left hand: 1. update `AxisAdaptiveLayer` module to align the global rotation axis of left hand. 2. update `AxisLayerFK` module to compose correct  axis angles for left hand

manotorch/axislayer.py
@@ -18,13 +18,17 @@ class AxisAdaptiveLayer(torch.nn.Module):
         self.parent_joints_mappings = [0, 5, 6, 0, 9, 10, 0, 17, 18, 0, 13, 14, 0, 1, 2]
         self.side = side
         if side == "right":
-            up_axis_base = np.vstack((np.array([[0, 1, 0]]).repeat(13, axis=0), np.array([[1, 1, 1]]).repeat(3, axis=0)))
+            up_axis_base = np.vstack(
+                (np.array([[0, 1, 0]]).repeat(13, axis=0), np.array([[1, 1, 1]]).repeat(3, axis=0))
+            )
         elif side == "left":
-            up_axis_base = np.vstack((np.array([[0, 1, 0]]).repeat(13, axis=0), np.array([[-1, 1, 1]]).repeat(3, axis=0)))
+            up_axis_base = np.vstack(
+                (np.array([[0, 1, 0]]).repeat(13, axis=0), np.array([[-1, 1, 1]]).repeat(3, axis=0))
+            )
         self.register_buffer("up_axis_base", torch.from_numpy(up_axis_base).float().unsqueeze(0))
 
     def forward(self, hand_joints, transf):
-        """ Compute the back (twist), up (spread), and left (bend) axes direction of the hand
+        """Compute the back (twist), up (spread), and left (bend) axes direction of the hand
         Args:
             hand_joints (torch.Tensor): (B, 16, 3)
             transf (torch.Tensor): (B, 16, 4, 4)
@@ -42,6 +46,7 @@ class AxisAdaptiveLayer(torch.nn.Module):
             b_axis_init = torch.tensor([1, 0, 0]).float().unsqueeze(0).unsqueeze(0).repeat(bs, 1, 1).to(b_axis.device)
         elif self.side == "left":
             b_axis_init = torch.tensor([-1, 0, 0]).float().unsqueeze(0).unsqueeze(0).repeat(bs, 1, 1).to(b_axis.device)
+            b_axis_init[:, :, 0] *= -1.0  # flip up direction for left hand
         b_axis = torch.cat((b_axis_init, b_axis), dim=1)  # (B, 16, 3)
 
         l_axis = torch.cross(b_axis, self.up_axis_base.expand(bs, 16, 3), dim=2)
@@ -61,6 +66,7 @@ class AxisLayerFK(Module):
         super(AxisLayerFK, self).__init__()
         self.transf_parent_mapping = [0, 0, 1, 2, 0, 4, 5, 0, 7, 8, 0, 10, 11, 0, 13, 14]
 
+        self.side = side
         tmpl_pose = torch.zeros(1, 48)
         tmpl_shape = torch.zeros(1, 10)
         tmpl_mano = ManoLayer(side=side, mano_assets_root=mano_assets_root)(tmpl_pose, tmpl_shape)
@@ -108,8 +114,9 @@ class AxisLayerFK(Module):
         Ta_par_chd = torch.matmul(T_g_a[:, self.transf_parent_mapping, ...].transpose(2, 3), T_g_a)  # (B, 16, 4, 4)
         Ra_par_chd = Ta_par_chd[:, :, :3, :3]  # (B, 16, 3, 3)
 
-        Ra_par_tmplchd = torch.matmul(self.TMPL_R_p_a[:, self.transf_parent_mapping, ...].transpose(2, 3),
-                                      self.TMPL_R_p_a)
+        Ra_par_tmplchd = torch.matmul(
+            self.TMPL_R_p_a[:, self.transf_parent_mapping, ...].transpose(2, 3), self.TMPL_R_p_a
+        )
         Ra_chd_tmplchd = torch.matmul(Ra_par_chd.transpose(2, 3), Ra_par_tmplchd)
         Ra_tmplchd_chd = Ra_chd_tmplchd.transpose(2, 3)
 
@@ -133,10 +140,16 @@ class AxisLayerFK(Module):
             torch.Tensor: mano pose (\theta) in the MANO original frame
         """
         ee_tmplchd_chd = angles  # (B, 16, 3)
+
+        if self.side == "left":
+            ee_tmplchd_chd[:, :, 0] *= -1.0
+            ee_tmplchd_chd[:, :, 1] *= -1.0
+
         Ra_tmplchd_chd = euler_angles_to_matrix(ee_tmplchd_chd, convention="XYZ")  # (B, 16, 3, 3)
 
-        Ra_par_tmplchd = torch.matmul(self.TMPL_R_p_a[:, self.transf_parent_mapping, ...].transpose(2, 3),
-                                      self.TMPL_R_p_a)
+        Ra_par_tmplchd = torch.matmul(
+            self.TMPL_R_p_a[:, self.transf_parent_mapping, ...].transpose(2, 3), self.TMPL_R_p_a
+        )
         Ra_par_chd = torch.matmul(Ra_par_tmplchd, Ra_tmplchd_chd)  # (B, 16, 3, 3)
 
         # chains
@@ -164,9 +177,11 @@ class AxisLayerFK(Module):
         return aa_p_par_chd
 
 
-@deprecated(deprecated_in="0.0.2",
-            removed_in="0.0.3",
-            details="This class is deprecated. Please use the new class 'AxisLayerFK' instead.")
+@deprecated(
+    deprecated_in="0.0.2",
+    removed_in="0.0.3",
+    details="This class is deprecated. Please use the new class 'AxisLayerFK' instead.",
+)
 class AxisLayer(Module):
 
     def __init__(self):